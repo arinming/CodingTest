af9760e98dc7ed273790174e759436979a3f709c
아딘컴\김아린 <jyandysys1@naver.com>
2023-02-27 18:44:20 +0900

    사칙연산

---
+++ Python/1008.py
@@ -0,0 +1,8 @@
+# 1008
+
+A, B = input().split()
+
+x = int(A)
+y = int(B)
+
+print(x/y)
---
+++ Python/10869.py
@@ -0,0 +1,12 @@
+# 10869
+
+A, B = input().split()
+
+x = int(A)
+y = int(B)
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x//y)
+print(x%y)
