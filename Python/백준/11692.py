39afc0fd9828807cef7a7127fc74a6c90d3ca01c
아딘컴\김아린 <jyandysys1@naver.com>
2023-04-20 01:02:05 +0900

    [백준] 11692 : 시간초과

---
+++ Python/백준/11692.py
@@ -0,0 +1,23 @@
+# 시그마함수
+
+# 약수 구하기 함수
+def Calc1(num1, i) :
+    num2 = num1 % i
+    if num2 == 0 :
+        return i
+    return 0
+
+# 약수에서 짝수 개수 세기
+def Calc2(num1) :
+    count = 0
+    ans = 0
+    for i in range(1, num1 + 1) :
+        count = Calc1(num1, i)
+        print(count)
+        if count != 0 and count % 2 == 0 :
+                ans += 1
+    print(ans)
+        
+num1 = int(input())
+
+Calc2(num1)
